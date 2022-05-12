d6f7957a8b8c68a295d00ae41d1b46bd0203e201
RomaTomashevskiy <romatomashevskiy@ukr.net>
2022-05-12 17:48:53 +0300

    домашка)

---
+++ src/js/02-timer.js
@@ -0,0 +1,107 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+    button: document.querySelector('button[data-start]'),
+    days: document.querySelector('[data-days]'),
+    hours: document.querySelector('[data-hours]'),
+    minutes: document.querySelector('[data-minutes]'),
+    seconds: document.querySelector('[data-seconds]')
+};
+
+refs.button.addEventListener('click', onBtnClick);
+
+refs.button.setAttribute('disabled', 'true');
+let isTimerRuning = false;
+let timerId = null;
+let userSelectedDate = null;
+
+// options for instance (date-view):
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {   
+    
+        if (selectedDates[0].getTime() <= Date.now()){
+            Notify.warning('Please choose a date in the future');  
+            return; 
+        };
+        
+        userSelectedDate = selectedDates[0];
+        refs.button.removeAttribute('disabled');
+    },
+    onOpen(){
+        
+        clearInterval(timerId);
+        isTimerRuning = false;
+        refs.button.setAttribute('disabled', 'true');
+
+        refs.days.textContent = '00';
+        refs.hours.textContent = '00';
+        refs.minutes.textContent = '00';
+        refs.seconds.textContent = '00';
+    }
+};
+
+//makes instance for date-view: 
+flatpickr('#datetime-picker', options);
+
+//Hendle Button Click:
+function onBtnClick(){
+    if(isTimerRuning) return;
+    
+    isTimerRuning = true;
+
+    timerId = setInterval(() => {
+        let currentDate = Date.now();
+        let convertUserSelectedDate = userSelectedDate.getTime();
+        let diference = convertUserSelectedDate - currentDate;
+
+        refs.days.textContent = convertMs(diference).days;
+        refs.hours.textContent = convertMs(diference).hours;
+        refs.minutes.textContent = convertMs(diference).minutes;
+        refs.seconds.textContent = convertMs(diference).seconds;
+        
+    }, 1000);
+};
+
+function convertMs(ms) {
+    // Number of milliseconds per unit of time
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    // Remaining days
+    const days = addLeadingZero(Math.floor(ms / day));
+    // Remaining hours
+    const hours = addLeadingZero(Math.floor((ms % day) / hour));
+    // Remaining minutes
+    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+    // Remaining seconds
+    const seconds = addLeadingZero(Math.floor((((ms % day) % hour) % minute) / second));
+
+    return { days, hours, minutes, seconds };
+};
+
+function addLeadingZero(value){
+    return String(value).padStart(2, '0');
+};
+
+//Shown(log) messsage view options:
+Notify.init({
+    width: '280px',
+    position: 'center-top',
+    distance: '25px',
+    timeout: 3000,
+    cssAnimationStyle: 'zoom',
+    warning: {
+        background: 'rgba(255, 72, 0, 0.945)',
+        textColor: '#fff',
+        notiflixIconColor: 'rgba(0,0,0,0.4)',
+    },
+});
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef =  document.querySelector('.form');
+
+formRef.addEventListener('submit', onSubmitClick);
+
+// submit handler
+function onSubmitClick(evt){
+  evt.preventDefault();
+
+  const {elements:{delay, step, amount}} = evt.currentTarget;
+
+  const amountPromises = Number(amount.value);
+  const stepOfDelay = Number(step.value);
+  let promiseDelay = Number(delay.value);
+  let position = 1;
+
+  for (let i = 1; i <= amountPromises; i+=1){
+
+  createPromise(position, promiseDelay)
+  .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+  })
+  .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+  });
+
+  position += 1;
+  promiseDelay += stepOfDelay;
+  };
+  
+  evt.currentTarget.reset();
+};
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  return new Promise((resolve, reject) => {
+
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay);
+  });
+};
+
+// Notify package options:
+Notify.init({
+  width: '300px',
+  timeout: 6000,
+});
